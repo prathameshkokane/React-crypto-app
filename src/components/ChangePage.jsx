cd18bf2115d9524ab42dc8daf3b7567b2917a8b0
Prathamesh Kokane <prathameshkokane8312@gmail.com>
2023-07-01 18:01:57 +0530

    first commit

---
+++ src/components/ChangePage.jsx
@@ -0,0 +1,23 @@
+// import React from "react";
+
+// const [page, setPage] = useState(1);
+// const ChangePage = (page) => {
+//   setPage(page);
+//   setLoading(true);
+// };
+
+// const btns = new Array(132).fill(1);
+// <HStack w={"full"} overflow={"auto"} p={"8"}>
+//   {btns.map((item, index) => (
+//     <Button
+//       key={index}
+//       bgColor={"blackAlpha.900"}
+//       color={"white"}
+//       onClick={() => changePage(index + 1)}
+//     >
+//       {index + 1}
+//     </Button>
+//   ))}
+// </HStack>;
+
+// export default ChangePage;
